c1d855a9b8256bd3f05b023e294d32a6e8b40437
LeulBayesa <leulbayesa@gmail.com>
2022-08-10 14:35:28 -0700

    prompt errors

--- get_error.c
+++ get_error.c
@@ -1,7 +1,8 @@
 #include "shell.h"
 
 /**
- * get_error - calls the error according the builtin, syntax or permission
+ * get_error - calls the error according the builtin,
+ * syntax or permission
  * @datash: data structure that contains arguments
  * @eval: error value
  * Return: error
